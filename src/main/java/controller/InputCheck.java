c1e07e79a1c4fed272a38b9728767616fe946854
Fabian_Eilber <fabian.eilber01@gmail.com>
2024-04-09 10:27:55 +0200

    InputCheck implemented

--- src/main/java/controller/InputCheck.java
+++ src/main/java/controller/InputCheck.java
@@ -1,4 +1,21 @@
 package controller;
-
+/**
+ * InputCheck prüft die Eingabe auf Korrektheit
+ * @author fe
+ * @version 1.0
+ */
 public class InputCheck {
+
+    public boolean isValidPhoneNumber(String input) {
+        if(input == null || input.isEmpty()) {
+            return false;
+        }
+
+        String cleanInput = input.replace(" ","");
+
+        if(cleanInput.length()<20 && cleanInput.length()>10){
+            return !cleanInput.matches(".*[a-zA-Z]*.");
+        }
+        return true;
+    }
 }
--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,7 +1,22 @@
 package controller;
 
+import java.util.Scanner;
+
+/**
+ * Controller nimmt eingabe entgegen und sorgt für die weitere Verarbeitung.
+ * @author fe
+ * @version 1.0
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte geben Sie eine Telefonnummer ein:");
+        String input = scanner.nextLine();
+
+        InputCheck inputCheck = new InputCheck();
+        if(!inputCheck.isValidPhoneNumber(input)){
+            System.out.println(input);
+        }
+
     }
 }
