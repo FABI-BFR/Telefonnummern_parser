68eb3f44f8d1768d5282f1a2ae02b2fb65a20ba2
Fabian_Eilber <fabian.eilber01@gmail.com>
2024-04-23 13:25:07 +0200

    Functions are commented

--- src/main/java/data/PhoneNumber.java
+++ src/main/java/data/PhoneNumber.java
@@ -24,31 +24,60 @@ public class PhoneNumber {
         this.extensionCode = extensionCode;
     }
 
+    /**
+     * Prints the parts of the phonenumber
+     *
+     * @return phonenumber as string
+     */
     @Override
     public String toString() {
         return "PhoneNumber {countryShort = '" + this.countryShort + "','countryCode='" + this.countryCode + "', areaCode='" + this.areaCode + "', numberCode='" + this.numberCode + "', extensionCode='" + this.extensionCode + "'}";
     }
 
+    /**
+     * Returns the phonenumber put in initially
+     * @return original phonenumber
+     */
     public String getOriginal() {
         return original;
     }
 
+    /**
+     * Returns the countryshort of a phonenumber
+     * @return countryshort of a phonenumber
+     */
     public String getCountryShort() {
         return countryShort;
     }
 
+    /**
+     * Returns the country code of a phonenumber
+     * @return country code of a phonenumber
+     */
     public String getCountryCode() {
         return countryCode;
     }
 
+    /**
+     * Returns the area code of a phonenumber
+     * @return area code of a phonenumber
+     */
     public String getAreaCode() {
         return areaCode;
     }
 
+    /**
+     * Returns the number of a phonenumber
+     * @return number of a phonenumber
+     */
     public String getNumberCode() {
         return numberCode;
     }
 
+    /**
+     * Returns the extension code of a phonenumber
+     * @return extension code of a phonenumber
+     */
     public String getExtensionCode() {
         return extensionCode;
     }
--- src/main/java/persistence/Database.java
+++ src/main/java/persistence/Database.java
@@ -18,16 +18,33 @@ public class Database implements ReadService, WriteService {
 
     private Map<Integer, PhoneNumber> list = new HashMap();
 
+    /**
+     * Returns the phonenumber for a certain id
+     *
+     * @param id phonenumberid
+     * @return phonenumber
+     */
     @Override
     public PhoneNumber getPhoneNumber(int id) {
         return this.list.get(id);
     }
 
+    /**
+     * Sets a phonenumber for a specific id
+     *
+     * @param id phonenumberid
+     * @param number phonenumber
+     */
     @Override
     public void setPhoneNumber(int id, PhoneNumber number) {
         this.list.put(id, number);
     }
 
+    /**
+     * Returns all phonenumber
+     *
+     * @return List of phonenumbers
+     */
     @Override
     public List<PhoneNumber> getAllNumbers() {
         List<PhoneNumber> numbers = new ArrayList<>();
